bf31f5fafb44a9517db17b9ef67a1e0c01d1846a
linzy <15521163524@163.com>
2022-09-14 14:27:44 +0800

    创建后端

---
+++ backend/src/main/java/com/linzy/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,15 @@
+package com.linzy.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/pk")
+public class BotInfoController {
+
+    @RequestMapping("/getBotInfo")
+    public String getBotInfo() {
+        return "hhhh";
+    }
+}
