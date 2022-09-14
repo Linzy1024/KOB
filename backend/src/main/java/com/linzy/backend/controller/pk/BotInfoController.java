f6e74eb975a52722f165932b6997574f8ece2918
linzy <15521163524.com>
2022-09-14 18:37:56 +0800

    前端骨架完成

--- backend/src/main/java/com/linzy/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/linzy/backend/controller/pk/BotInfoController.java
@@ -3,13 +3,19 @@ package com.linzy.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/pk")
 public class BotInfoController {
 
     @RequestMapping("/getBotInfo")
-    public String getBotInfo() {
-        return "hhhh";
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot = new HashMap<>();
+        bot.put("name", "tiger");
+        bot.put("rating", "1500");
+        return bot;
     }
 }
